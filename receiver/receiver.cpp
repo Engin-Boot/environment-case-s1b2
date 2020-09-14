ae6f755629fac7b1163c2533041b1f3efd75e05d
shivanigoyal <38599629+shivanigoyal236@users.noreply.github.com>
2020-09-14 23:58:20 +0530

    Update receiver.cpp

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -7,84 +7,88 @@ using namespace std;
 class AlertMessage
 {
     public:
-    void RaiseAlert(string& message)
+    string RaiseAlert(string message,string valuename,string level)
     {
-        cout<<message<<endl;
+       string s;
+       s=message+"!!"+valuename+" is "+level;
+       return s;
     }
 };
+class CheckValues
+{
+   private:
+   AlertMessage *alert;
+   public:
+   CheckValues(){}
+   string CheckValuesForAlert(float value,string ValueName,float HighValueForAlert,float HighValueForWarning,float LowValueForAlert,float LowValueForWarning)
+  {
+   
+     string s="";
+     if(value>HighValueForAlert)
+     {
+        s=alert->RaiseAlert("Alert",ValueName,"Very high");
+	 }
+	 else if(value>HighValueForWarning)
+	 {
+	 	s=alert->RaiseAlert("Warning",ValueName,"high");
+	 }
+	 else if(value<LowValueForAlert)
+	 {
+	 	s=alert->RaiseAlert("Alert",ValueName,"very low");
+	 }
+     else if(value<LowValueForWarning)
+     {
+     	s=alert->RaiseAlert("Warning",ValueName,"low");
+	 }
+     return s;
+  }
+};
+
+class PrintMessage
+{   
+    public:
+      void PrintMessageOnConsole(string messageForTem,string messageForHumi)
+        {
+           cout<<messageForTem<<endl;
+           cout<<messageForHumi<<endl;
+        }
+};
+
 class CheckForWarningAndAlert
 {     
-     private:
-      AlertMessage *alert;
+ private:
+    CheckValues check;
+    PrintMessage *print;
+    float ErrorHighValueForTem=40;
+    float ErrorLowValueForTem=0;
+    float WarningHighValueForTem=37;
+    float WarningLowValueFortem=4;
+    float ErrorHighValueForHumi=90;
+    float WarningHighValueForHumi=70;
     public:
     CheckForWarningAndAlert(){}
-    CheckForWarningAndAlert(float tempreature,float humidity)
+     CheckForWarningAndAlert(float tempreature,float humidity)
      {
-           
-         AlertAndWarningForHumidity(humidity);
-         AlertAndWarningForTempreature(tempreature);
+         
+     string messageForTem=check.CheckValuesForAlert(tempreature,"tempreature",ErrorHighValueForTem
+     ,WarningHighValueForTem,ErrorLowValueForTem,WarningLowValueFortem);
+     string messageForHumi=check.CheckValuesForAlert(humidity,"humidity",ErrorHighValueForHumi
+     ,WarningHighValueForHumi,0,0);
+     print->PrintMessageOnConsole(messageForTem,messageForHumi);
      }
-    void AlertAndWarningForHumidity(float humidity)
-    {    string message;
-       if(humidity>90)
-       {     
-           message="ERROR!!humidity is high";
-           alert->RaiseAlert(message);
-       }
-       else if(humidity>70)
-       {
-
-       message="WARNING!!humidity is high";
-           alert->RaiseAlert(message);}
-
-    }
-      void AlertAndWarningForTempreature(float tempreature)
-   {    
-          if(tempreature<4)
-          AlertForLowTempreature(tempreature);
-          else if(tempreature>37)
-          AlertForHighTempreature(tempreature);
-   }
-   void  AlertForLowTempreature(float tempreature)
-   {
-       if(tempreature<0)
-       {
-           string message="ERROR!!tempreature is low";
-           alert->RaiseAlert(message);
-       }
-       else
-       {
-           string message="WARNING!!humidity is low";
-           alert->RaiseAlert(message);
-       }
-   }
-    void AlertForHighTempreature(float tempreature)
-    {
-         if(tempreature>40)
-         {
-             string message="ERROR!!tempreature is very high";
-           alert->RaiseAlert(message);
-         }
-       else
-       {
-           string message="WARNING!!Tempreature is high";
-           alert->RaiseAlert(message);
-       }
-    }
 };
 
-void extractValuesFromString(string line) 
+void extractValuesFromStringAndSendValuesForChecking(string line) 
 { 
     stringstream ss(line); 
         while(ss.good())
-         {
+        {
          	string tempreaturestring,humiditystring;
          	getline(ss,tempreaturestring,' ');
          	getline(ss,humiditystring,'\n');
-         	//cout<<tempreaturestring<<" "<<humiditystring;
-         		CheckForWarningAndAlert obj(stof(tempreaturestring),stof(humiditystring));
-} 
-		 }
+         	CheckForWarningAndAlert obj(stof(tempreaturestring),stof(humiditystring));
+        } 
+}
        
 
  int main()
@@ -93,7 +97,7 @@ void extractValuesFromString(string line)
           string line="";
     while(getline(cin,line))
     {   
-           extractValuesFromString(line);
+        extractValuesFromStringAndSendValuesForChecking(line);
     }
     return 0;
 }
