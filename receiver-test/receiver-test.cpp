5453a570a0a06ff1b12e8568ffb53976313129ae
shivanigoyal <38599629+shivanigoyal236@users.noreply.github.com>
2020-09-15 14:46:48 +0530

    Update receiver-test.cpp

--- receiver-test/receiver-test.cpp
+++ receiver-test/receiver-test.cpp
@@ -9,19 +9,19 @@ CheckValues check;
 string tempreature="tempreature",humidity=humidity;
 TEST_CASE("raise error and warning when tempreature is greater than alert level")
 {   
-    REQUIRE(check.checkforhighvalues(42,tempreature,40,37)==("ALERT!! tempreature is very high"));
-    REQUIRE(check.checkforhighvalues(42,tempreature,40,37)==("ALERT!! tempreature is high"));
+    REQUIRE(check.checkforhighvalues(42,tempreature,40,37)==("ALERT!!tempreature is very high"));
+    REQUIRE(check.checkforhighvalues(42,tempreature,40,37)==("ALERT!!tempreature is high"));
     
 }
 TEST_CASE("raise error and warning when tempreature is lower than alert level")
 {
-     REQUIRE(check.checkforlowvalues(-0.92,tempreature,0,4)==("ALERT!! tempreature is very low"));
-     REQUIRE(check.checkforlowvalues(2,tempreature,0,4)==("WARNING!! tempreature is low"));
+     REQUIRE(check.checkforlowvalues(-0.92,tempreature,0,4)==("ALERT!!tempreature is very low"));
+     REQUIRE(check.checkforlowvalues(2,tempreature,0,4)==("WARNING!!tempreature is low"));
 }
 TEST_CASE("raise error and warning when humidity is greater than alert level")
 {
-    REQUIRE(check.checkforhighvalues(93,humidity,90,70)==("ALERT!! humidity is very high"));
-    REQUIRE(check.checkforhighvalues(73,humidity,90,70)==("ALERT!! humidity is high"));
+    REQUIRE(check.checkforhighvalues(93,humidity,90,70)==("ALERT!!humidity is very high"));
+    REQUIRE(check.checkforhighvalues(73,humidity,90,70)==("ALERT!!humidity is high"));
 }
 TEST_CASE("Do nothing when tempreature is in normal conditions")
 {      
