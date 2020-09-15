d1db03ae536653c55f39166ad0c5208f196377f2
Rajmalbasha <rajmal046@gmail.com>
2020-09-15 22:38:18 +0530

    sender_test files

---
+++ sender-test/sender_test.cpp
@@ -0,0 +1,30 @@
+#define CATCH_CONFIG_MAIN
+
+#include<iostream>
+#include<vector>
+#include "../AllFiles/catch.hpp"
+#include "../sender/sender.h"
+
+using namespace std;
+
+TEST_CASE("test case for modify data")
+{     vector<string>data1={"21","34"};
+       vector<string>data2={"21@","23.78"};
+       vector<string>data3={"21","34"};
+       vector<string>data4={"Invalid","Invalid"};
+    REQUIRE(modifyInvalidData(data1)==data3);
+    REQUIRE(modifyInvalidData(data2)==data4);
+
+}
+TEST_CASE("for checkProcessedRowDataIsAInvalidEntry")
+{   const vector<vector <string> >row={  {"21","78"},{"Invalid","Invalid"}};
+  
+   REQUIRE(checkProcessedRowDataIsAInvalidEntry (row,0)==false);
+   REQUIRE(checkProcessedRowDataIsAInvalidEntry (row,1)==true);
+}
+
+TEST_CASE("for processInvalidEntries")
+{   vector<vector <string> >fetchedData={{"67","45"},{"98.45@","@@"},{"89","@3"}};
+    vector<vector <string> > expectedData= {{"67","45"},{"Invalid","Invalid"},{"Invalid","Invalid"}};
+    REQUIRE(processInvalidEntries(fetchedData)==expectedData);
+}
