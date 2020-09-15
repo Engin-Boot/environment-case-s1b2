4d17799d8b661e185edf5584d7e29b2a2d2201f1
shivanigoyal <38599629+shivanigoyal236@users.noreply.github.com>
2020-09-15 13:35:40 +0530

    Update receiver-test.cpp

--- receiver/receiver-test.cpp
+++ receiver/receiver-test.cpp
@@ -1,7 +1,7 @@
-#define CATCH_CONFIG_RUNNER
+#define CATCH_CONFIG_MAIN
 
 #include<bits/stdc++.h>
-#include "catch.hpp"
+#include "../AllFiles/catch.hpp"
 #include "receiver.h"
 using namespace std;
 
@@ -32,26 +32,4 @@ TEST_CASE("Do nothing when humidity is in normal conditions")
 {
     REQUIRE(check.CheckValuesForAlert(7,humidity,90,70,0,0)=="");
     REQUIRE(check.CheckValuesForAlert(34,humidity,90,70,0,0)=="");
-}
-void extractValuesFromStringAndSendValuesForChecking(string line) 
-{ 
-    stringstream ss(line); 
-        while(ss.good())
-        {
-         	string tempreaturestring,humiditystring;
-         	getline(ss,tempreaturestring,' ');
-         	getline(ss,humiditystring,'\n');
-         	CheckForWarningAndAlert obj(stof(tempreaturestring),stof(humiditystring));
-        } 
-}
-       
- int main()
-{    
-    
-          string line="";
-    while(getline(cin,line))
-    {   
-        extractValuesFromStringAndSendValuesForChecking(line);
-    }
-    return 0;
 }
